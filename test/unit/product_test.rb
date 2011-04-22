1f5266be8e1d11c82bf7290f8cb54f0f6b501e5f
Tom Clemente <underscor_e@yahoo.com>
2011-04-22 20:30:29 +0800

    validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
+  fixtures :products
+  test "product is not valid without a unique title -il8n" do
+    product = Product.new(:title	=> products(:ruby).title,
+			  :description	=> "yyy",
+			  :price	=> 1,
+			  :image_url	=> "fred.gif")
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+		 product.errors[:title].join('; ')
+  end
+
+  test "product attributes must not be empty" do
+	product=Product.new
+	assert product.invalid?
+	assert product.errors[:title].any?
+	assert product.errors[:description].any?
+	assert product.errors[:price].any?
+	assert product.errors[:image_url].any?
+	
+     end
+test "product price must be positive" do
+  product = Product.new(:title		=> "My Book Title",
+			:description 	=> "yyy",
+			:image_url	=> "zzz.jpg")
+  product.price = -1
+  assert product.invalid?
+  assert_equal "must be greater than or equal to 0.01",
+    product.errors[:price].join('; ')
+
+  product.price=0
+  assert product.invalid?
+  assert_equal "must be greater than or equal to 0.01",
+    product.errors[:price].join('; ')
+
+  product.price = 1
+  assert product.valid?
+end
+
   test "the truth" do
     assert true
   end
